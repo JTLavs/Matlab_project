2f12c3671f312cbfad2c49308f18f5afb52a5801
Daniel <danielcooke1996@gmail.com>
2018-03-05 22:43:52 +0000

    Moved CVError

--- Classifiers/cvError.m
+++ Classifiers/cvError.m
@@ -6,8 +6,34 @@
 
 %  trainingData -  a set of labelled training data
 %  kFolds - the number of partitions to make in each set
-%  classifier - a function handle to reference the classifier
-%%
-function cvErr = cvError(trainingData, kFolds, classifier)
+%  classifier - a reference to a classifier object.
+% 
+%
+%% - Dan
+function cvErr = cvError(trainingData, trainingLabels, kFolds, classifier)
+    indices = crossvalind('KFold', size(trainingData,1), kFolds);
     
+  
+    cvErr = [];
+    for i = 1:kFolds
+        testData = trainingData(indices == i);
+        testLabels = trainingLabels(indices == i);
+        
+        trainData = trainingData(indices ~= i);
+        trainLabels = trainingLabels(indices ~= i);
+        
+        classifier.train(trainData, trainLabels);
+        predictions = [];
+        for j = 1:size(testData,1)
+            newClass = classifier.test(testData(j,:))
+            predictions = [predictions; newClass]
+        end
+      
+        numCorrect = (testLabels == predictions);
+        numCorrect = size(numCorrect(numCorrect == 1),1);
+        cvErr = [cvErr; 1 - (numCorrect /size(testData,1))];
+        
+    end
+    
+    cvErr = mean (cvErr);
 end
