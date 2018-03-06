f35017f91ad5db9ebf16106c75618c7012987b54
Daniel <danielcooke1996@gmail.com>
2018-03-06 20:31:54 +0000

    Neural Network added

--- Feature Extraction/FeatureExtraction.m
+++ Feature Extraction/FeatureExtraction.m
@@ -14,7 +14,6 @@ svm = classifier(@SVMTrain, @SVMTest)
 nn = classifier(@NNTrain, @NNTest)
 
 
-
 %% Load training images
 [training_images, training_labels] = loadPedestrianDatabase(TRAINING_DATASET_PATH, 10);
 pedestrians = find(training_labels == 1);
@@ -57,17 +56,7 @@ others = find(test_labels == -1);
 test_images= [test_images(pedestrians,:); test_images(others,:)]; 
 test_labels= [test_labels(pedestrians); test_labels(others)];
 
-%% Extract hog feature vectors for test images
-test_features = []
-for i=1:size(test_images, 1)
 
-   Im = reshape(test_images(i,:),160,96);
-   [ImEdEx, ImIhor, ImIver] =  edgeExtraction(Im,maskA, maskB);
-   hog = hog_feature_vector(ImIhor);
-   
-   test_features = [test_features; hog];
-   % TODO: classify each test image here using hog vector
-end
+
 %%
-comparison = (test_labels==classificationResult);
-Accuracy = sum(comparison)/length(comparison)
+ err = getErrorRate(svm, test_images, test_labels)
