8dfb0f3e1efde7ecbd4f7825e47270f0cd5097df
Daniel Cooke <danielcooke1996@gmail.com>
2018-03-20 14:51:36 +0000

    Add files via upload

--- TrainDetectorModel.m
+++ TrainDetectorModel.m
@@ -1,12 +1,13 @@
 function detectorModel = TrainDetectorModel()
 
     TRAINING_DATASET_PATH = 'Data/pedestrian_train.cdataset';
+    TEST_DATASET_PATH = 'Data/pedestrian_test.cdataset';
     knn = classifier(@KNNTrain,  @KNNTest);
     svm = classifier(@SVMTrain, @SVMTest);
     nn = classifier(@NNTrain, @NNTest);
     
     %% Load training images
-    [training_images, training_labels] = loadPedestrianDatabase(TRAINING_DATASET_PATH, 10);
+    [training_images, training_labels] = loadPedestrianDatabase(TRAINING_DATASET_PATH, 1);
     pedestrians = find(training_labels == 1);
     others = find(training_labels == -1);
 
@@ -14,6 +15,14 @@ function detectorModel = TrainDetectorModel()
     training_images= [training_images(pedestrians,:); training_images(others,:)];
     training_labels = [training_labels(pedestrians,:); training_labels(others,:)];
     
+    %% Load tets images
+     [test_images, test_labels] = loadPedestrianDatabase(TEST_DATASET_PATH, 1);
+    pedestrians = find(test_labels == 1);
+    others = find(test_labels == -1);
+
+
+    test_images= [test_images(pedestrians,:); test_images(others,:)];
+    test_labels = [test_labels(pedestrians,:); test_labels(others,:)];
     %% Setup masks for edge extraction
 
     maskA = ones(3);
@@ -26,19 +35,25 @@ function detectorModel = TrainDetectorModel()
 
     %% Extract hog feature vectors for training images
     [tr_hogEdEx, tr_hogHor, tr_hogVer, tr_hogIm] = getHogFeatures(training_images, maskA, maskB);
+   
     
     %% Train classifier models
 
-    knn.train(tr_hogEdEx, training_labels);
+   
     svm.train(tr_hogEdEx, training_labels);
-    nn.train(tr_hogEdEx, training_labels);
+   
     
-  
+ 
     detectorModel = struct;
     detectorModel.knn = knn;
     detectorModel.nn = nn;
     detectorModel.svm = svm;
 
-    
+    %% Get Test features
+    [te_hogEdEx, te_hogHor, te_hogVer, te_hogIm] = getHogFeatures(test_images, maskA, maskB);
+   
+    %% GET ERROR
+
+    getErrorRate(detectorModel.ans.svm, te_hogEdEx, test_labels);
 
 end
