69dd334839a4f89041e1dd9e79c20a8beee6beaf
James Lavery <mickey_noo@hotmail.com>
2018-03-22 19:52:30 +0000

    Delete EuclideanDistance.m

--- EuclideanDistance.m
+++
@@ -1,8 +0,0 @@
-function dEuc = EuclideanDistance( sample1, sample2 )
-    total = 0;
-    for t=1:size(sample2,2)
-        total = total +  (sample1(t) - sample2(t))^2;
-    end
-    dEuc = sqrt(double(total));
-end
-
